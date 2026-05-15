2930f33f2bbcfb20fa659f4008880ab0e154f0da
sandeep <sandeepmishra898k@gmail.com>
2026-05-15 17:07:25 +0530

    5. Factors (Prime Factorization)

---
+++ App/src/Factors.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Prime Factors are:");
+
+        for (int i = 2; i * i <= n; i++) {
+
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n = n / i;
+            }
+        }
+
+        if (n > 1)
+            System.out.print(n);
+    }
+}
